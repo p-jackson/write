b65b499abcb85f73cb171c72ef4372524c15ae57
Philip Jackson <p-jackson@live.com>
2015-01-24 01:22:19 +1300

    Added a grunt task for building the distributables.

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,22 +7,56 @@ module.exports = function(grunt) {
     less: {
       development: {
         options: {
-          paths: ['./less'],
-          yuicompress: true
+          paths: ['./less']
         },
         files: {
           './css/main.css': './less/main.less'
         }
+      },
+      dist: {
+        options: {
+          paths: ['./less'],
+          compress: true
+        },
+        files: {
+          './dist/css/main.css': './less/main.less'
+        }
       }
     },
+    requirejs: {
+      dist: {
+        options: {
+          baseUrl: './js',
+          name: 'main',
+          out: './dist/js/main.js',
+
+          paths: {
+            knockout: 'empty:'
+          }
+        }
+      }
+    },
+    copy: {
+      dist: {
+        files: [
+          { src: ['./img/*'], dest: './dist/' },
+          { src: ['./index.html'], dest: './dist/' }
+        ],
+      },
+    },
+    clean: [ './dist' ],
     watch: {
       files: './less/*',
-      tasks: ['less']
+      tasks: [ 'less:development' ]
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
 
-  grunt.registerTask('default','watch');
+  grunt.registerTask('default', 'watch');
+  grunt.registerTask('dist', [ 'clean', 'copy:dist', 'requirejs:dist', 'less:dist' ]);
 };
